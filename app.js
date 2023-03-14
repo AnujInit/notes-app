c143416cbe0fe16e7d4a0b63ab67259606f43d7a
Anuj <akg.cs.engg@gmail.com>
2023-03-14 23:31:52 +0530

    Added CRUD Operations using Command line args

--- app.js
+++ app.js
@@ -1,1 +1,55 @@
-console.log('Welcome to NotesApp');
+const yargs = require('yargs');
+
+//Customize yargs version
+yargs.version('1.1.0');
+
+//Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler: function (argv) {
+    console.log(`Title: ${argv.title}, Body: ${argv.body}`);
+  },
+});
+
+//Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function () {
+    console.log(`'Removing the note'`);
+  },
+});
+
+//Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List your notes',
+  handler: function () {
+    console.log('Listing out all notes');
+  },
+});
+
+//Create read command
+yargs.command({
+  command: 'read',
+  describe: 'read a note',
+  handler: function () {
+    console.log('reading a note');
+  },
+});
+
+//console.log(yargs.argv);
+yargs.parse();
